5226ec7e20764df7774821e73dd34f4453d391bf
Chris Wilson <203455+cbw@users.noreply.github.com>
2022-10-02 09:28:16 -0700

    Tweaks

--- go.sh
+++ go.sh
@@ -7,16 +7,21 @@ abort() {
   exit 1
 }
 
+echo "Starting bootstrap process..."
+
 if [ -z "${BASH_VERSION:-}" ]
 then
   abort "Bash is required to interpret this script."
 fi
 
+echo "- Creating ~/.bootstrap..."
 mkdir -p ~/.bootstrap
+echo "- Fetching scripts..."
 curl -sSLo ~/.bootstrap/bootstrap.sh https://raw.githubusercontent.com/cbw/bootstrap/main/bootstrap.sh
 curl -sSLo ~/.bootstrap/bootstrap.yaml https://raw.githubusercontent.com/cbw/bootstrap/main/bootstrap.yaml
 curl -sSLo ~/.bootstrap/requirements.yaml https://raw.githubusercontent.com/cbw/bootstrap/main/requirements.yaml
 curl -sSLo ~/.bootstrap/aws.env https://raw.githubusercontent.com/cbw/bootstrap/main/aws.env
 chmod 755 ~/.bootstrap/bootstrap.sh
 
+echo "- Executing bootstrap:"
 (cd ~/.bootstrap; ./bootstrap.sh)
