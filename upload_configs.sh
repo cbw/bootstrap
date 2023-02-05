b27d983225ccb99bdbd46d3a7215e81521d9f47f
Chris Wilson <203455+cbw@users.noreply.github.com>
2023-02-05 10:59:45 -0800

    gitconfig

--- upload_configs.sh
+++ upload_configs.sh
@@ -11,3 +11,9 @@ op run --env-file="aws.env" -- aws s3 cp ~/.zshrc s3://cbw-app-data/zsh/zshrc
 
 # Upload current ssh_config file
 op run --env-file="aws.env" -- aws s3 cp  ~/.ssh/config s3://cbw-app-data/ssh/config 
+
+# Upload current .gitconfig file
+op run --env-file="aws.env" -- aws s3 cp  ~/.gitconfig s3://cbw-app-data/git/gitconfig 
+
+
+# TODO: package and upload Alfred configs: s3://cbw-app-data/alfred/alfred.zip
